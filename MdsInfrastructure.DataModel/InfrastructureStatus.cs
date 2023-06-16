7b4ffad4fc4e140d1fa0f386a6ce7f8ade7d2de7
Calin Dobos <calin.dobos@qwebsolutions.eu>
2023-06-16 23:37:13 +0300

    Initialize InfrastructureStatus properties to avoid crashes.

--- MdsInfrastructure.DataModel/InfrastructureStatus.cs
+++ MdsInfrastructure.DataModel/InfrastructureStatus.cs
@@ -8,16 +8,16 @@ namespace MdsInfrastructure
         public InfrastructureConfiguration InfrastructureConfiguration { get; set; } = new();
         public List<InfrastructureNode> InfrastructureNodes { get; set; } = new();
         public List<MachineStatus> HealthStatus { get; set; } = new();
-        public Deployment Deployment { get; set; }
-        public List<InfrastructureEvent> InfrastructureEvents { get; set; }
+        public Deployment Deployment { get; set; } = new();
+        public List<InfrastructureEvent> InfrastructureEvents { get; set; } = new();
         public string SchemaValidationMessage { get; set; } = string.Empty;
 
-        public Metapsi.Ui.User User { get; set; }
+        public Metapsi.Ui.User User { get; set; } = new();
     }
 
     public class ApplicationStatus
     {
-        public string ApplicationName { get; set; }
-        public InfrastructureStatus InfrastructureStatus { get; set; }
-    }   
+        public string ApplicationName { get; set; } = string.Empty;
+        public InfrastructureStatus InfrastructureStatus { get; set; } = new();
+    }
 }
