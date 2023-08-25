d75aa986863964a6817d5e716de42a182640baf8
Calin Dobos <calin.dobos@qwebsolutions.eu>
2023-08-25 16:43:39 +0300

    Remove test Choices.

--- MdsInfrastructure.Render/InfrastructureStatus.cs
+++ MdsInfrastructure.Render/InfrastructureStatus.cs
@@ -35,36 +35,6 @@ namespace MdsInfrastructure.Render
 
         public static Var<HyperNode> Render(BlockBuilder b, MdsInfrastructure.InfrastructureStatus dataModel)
         {
-            List<Choice> choices = new List<Choice>()
-            {
-                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-                                new Choice(){label = "First", value = "1" },
-                new Choice(){label = "Second", value = "2" },
-
-
-            };
-
-            var props = b.NewObj<Metapsi.ChoicesJs.Props>(b =>
-            {
-                //b.Set(x => x.maxItemCount, 1);
-                b.Set(x => x.choices, b.Const(choices));
-                b.Set(x => x.removeItemButton, true);
-            });
 
             if (!string.IsNullOrEmpty(dataModel.SchemaValidationMessage))
             {
@@ -77,26 +47,12 @@ namespace MdsInfrastructure.Render
             {
                 return b.Text("No deployment yet! The infrastructure is not running any service!").As<HyperNode>();
             }
-
+            
             var page = b.Div("flex flex-col space-y-4");
 
             int totalServices = dataModel.Deployment.GetDeployedServices().Count();
             int totalNodes = dataModel.Deployment.GetDeployedServices().Select(x => x.NodeName).Distinct().Count();
 
-            var choicesText = b.Add(page, b.ChoicesText(props));
-            Metapsi.ChoicesJs.Event.MultiBindTo<MdsInfrastructure.InfrastructureStatus>(b, choicesText, x => x.InputChoiceTest);
-            var test = b.Add(page, b.Text("Test!"));
-
-            b.SetOnClick(test, b.MakeAction((BlockBuilder b, Var<object> o) =>
-            {
-                return b.Clone(o);
-            }));
-
-            var selectOne = b.Add(page, b.ChoicesSelectOne(props));
-            Metapsi.ChoicesJs.Event.SingleBindTo<MdsInfrastructure.InfrastructureStatus>(b, selectOne, x => x.SingleChoiceTest);
-            
-            var selectMultiple = b.Add(page, b.ChoicesSelectMultiple(props));
-            Metapsi.ChoicesJs.Event.MultiBindTo<MdsInfrastructure.InfrastructureStatus>(b, selectMultiple, x => x.MultiChoiceTest);
 
             b.Add(
                 page,
